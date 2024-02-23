24b40d90f5efae89802906689fdef6357a914a91
Hoodahashem <mahmoudhashem335@gmail.com>
2024-02-23 19:21:27 +0200

    improve the count!!!

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,50 @@
 #!/usr/bin/python3
 from flask import Flask, Blueprint
 from api.v1.views import app_views
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
+
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     """Return status"""
     return {"status": "OK"}
+
+
+@app_views.route("/stats", strict_slashes=False)
+def stats():
+    """Returns counts for objects"""
+    objs_dict = storage.all()
+    count_dict = {}
+
+    classes = [Amenity, City, Place, Review, State, User]
+
+    for i in classes:
+        count = storage.count(i)
+
+        if count == 0:
+            continue
+        if i == Amenity:
+            count_dict["amenities"] = count
+
+        elif i == City:
+            count_dict["cities"] = count
+
+        elif i == Place:
+            count_dict["places"] = count
+
+        elif i == Review:
+            count_dict["reviews"] = count
+
+        elif i == State:
+            count_dict["states"] = count
+
+        elif i == User:
+            count_dict["users"] = count
+
+    return (count_dict)
