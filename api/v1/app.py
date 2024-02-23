8333efef0fe9f4839c93416bbf842e1039f9dcfd
Hoodahashem <mahmoudhashem335@gmail.com>
2024-02-24 01:59:59 +0200

    styling is done!!

--- api/v1/app.py
+++ api/v1/app.py
@@ -20,6 +20,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 cros = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     """teardown"""
