d0f6e8e38e95a0ce51dfef2e82f134bb53d713a6
Hoodahashem <mahmoudhashem335@gmail.com>
2024-02-24 01:59:03 +0200

    final task!!

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,7 @@
 from flask import Flask, blueprints, jsonify
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
 import os
 
 HBNB_API_HOST = os.getenv('HBNB_API_HOST')
@@ -17,7 +18,7 @@ if HBNB_API_PORT is None:
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-
+cros = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def teardown_db(exception):
