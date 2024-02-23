1403dccdfda76068bf6822b21d5a77f4567b8bd8
Hoodahashem <mahmoudhashem335@gmail.com>
2024-02-24 01:10:39 +0200

    test get states function!!

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -7,7 +7,6 @@ from models.city import City
 from models import storage
 from api.v1.views import app_views
 
-
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_states():
     """ Retrieves the list of all State objects """
@@ -17,52 +16,3 @@ def get_states():
         state_list.append(state.to_dict())
     return jsonify(state_list)
 
-
-@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
-def get_state(state_id):
-    """ Retrieves a State object """
-    state = storage.get(State, state_id)
-    if state is None:
-        abort(404)
-    return jsonify(state.to_dict())
-
-
-@app_views.route('/states/<state_id>', methods=['DELETE'],
-                 strict_slashes=False)
-def delete_state(state_id):
-    """ Deletes a State object """
-    state = storage.get(State, state_id)
-    if state is None:
-        abort(404)
-    storage.delete(state)
-    storage.save()
-    return jsonify({}), 200
-
-
-@app_views.route('/states', methods=['POST'], strict_slashes=False)
-def create_state():
-    """ Creates a State """
-    data = request.get_json()
-    if data is None:
-        return "Not a JSON", 400
-    if 'name' not in data:
-        return "Missing name", 400
-    new_state = State(**data)
-    new_state.save()
-    return jsonify(new_state.to_dict()), 201
-
-
-@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
-def update_state(state_id):
-    """ Updates a State object """
-    data = request.get_json()
-    if data is None:
-        return "Not a JSON", 400
-    state = storage.get(State, state_id)
-    if state is None:
-        abort(404)
-    for key, value in data.items():
-        if key not in ['id', 'created_at', 'updated_at']:
-            setattr(state, key, value)
-    storage.save()
-    return jsonify(state.to_dict()), 200
