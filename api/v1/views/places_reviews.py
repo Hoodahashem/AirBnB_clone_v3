c2fbbfb8a149f8a18d039c2e42d5ae0a9d0d002c
Hoodahashem <mahmoudhashem335@gmail.com>
2024-02-24 01:57:11 +0200

    styling is done!

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -76,8 +76,8 @@ def put_review(review_id):
         abort(400, description="Not a JSON")
     data = request.get_json()
     for key, value in data.items():
-        if key not in ['id', 'user_id', 'place_id', 'created_at', 'updated_at']:
+        if key not in ['id', 'user_id',
+                       'place_id', 'created_at', 'updated_at']:
             setattr(review, key, value)
     review.save()
     return make_response(jsonify(review.to_dict()), 200)
-
