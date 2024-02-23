fae38ed132b9967adca9f6ac7cef4da640647678
Hoodahashem <mahmoudhashem335@gmail.com>
2024-02-23 14:47:40 +0200

    again

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from flask import Flask, Blueprint
-from api.v1.views import app_views
+from views import app_views
 
 @app_views.route('/status', strict_slashes=False)
 def status():
